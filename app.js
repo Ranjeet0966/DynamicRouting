8f0b8fd71cd5bde36e7ac075399b97b5deee42f1
Ranjeet Kumar Yadav <innovativeranjeet@gmail.com>
2023-09-03 06:45:40 -0400

    retrieving data from sql

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+
+
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -22,3 +25,4 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(3000);
+
--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,15 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
-};
+  product
+  .save()
+  .then(()=>{
+    res.redirect('/');
+  })
+  .catch(err => console.log(err));
+  
+}; 
+
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
@@ -66,6 +72,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId)
+  .then(([products])=>{
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err));
+ 
 }
+
