ae7c79f2e9f5bfb0fe9a1d85faa311b4c8b6b702
Ranjeet Kumar Yadav <139030113+Ranjeet0966@users.noreply.github.com>
2024-04-06 08:42:22 +0530

    USING MONGODB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,34 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize =  new Sequelize('node-complete', 'root', 'Ganguly@123',{
-    dialect: 'mysql',
-    host: 'localhost'
-});
+// const sequelize =  new Sequelize('node-complete', 'root', 'Ganguly@123',{
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+const Mongodb = require('mongodb');
+// require('dotenv').config();
+const MongoClient =Mongodb.MongoClient;
+let _db;
+
+const mongoConnect=async (callback)=>{
+    try{
+        const client = await MongoClient.connect('mongodb+srv://rahul:rahul@atlascluster.sw1lvwy.mongodb.net/');
+        _db=client.db('shop');
+        console.log("Connected")
+        callback();
+    }
+    catch(e){
+        console.log(e.message)
+    }
+   
+}
+exports.getDb=()=>{
+    if(_db){
+        return _db;
+    }
+    else{
+        throw 'database not connected'
+    }
+}
+exports.mongoConnect=mongoConnect;
