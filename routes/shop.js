c6180d7da8c90758af06f028beb1073053f1d557
Ranjeet Kumar Yadav <139030113+Ranjeet0966@users.noreply.github.com>
2024-04-10 23:25:14 +0530

    getting orderPlaced

--- routes/shop.js
+++ routes/shop.js
@@ -23,9 +23,9 @@ router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.post('/cart', shopController.postCart);
 
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 // router.get('/checkout', shopController.getCheckout);
-
+router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
