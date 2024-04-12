4cce583e3f7dc721a0e78b89832a55866ceb94df
Ranjeet Kumar Yadav <139030113+Ranjeet0966@users.noreply.github.com>
2024-04-12 19:47:52 +0530

    getting order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 
 
 const Product = require('../models/product');
+const Order = require('../models/order');
+
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -94,9 +96,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+  .populate('cart.items.productId')
+  .then(user=>{
+    const products = user.cart.items.map(i=>{
+      return {quantity:i.quantity, product:i.productId}
+    })
+    const order= new Order({
+      user:{
+        name:req.user.name,
+      userId: req.user
+      },
+  products: products
+    });
+    return order.save();
+  })
     .then(result => {
       res.redirect('/orders');
     })
--- routes/shop.js
+++ routes/shop.js
@@ -25,7 +25,7 @@ router.post('/cart', shopController.postCart);
 
 // router.get('/orders', shopController.getOrders);
 
-// // router.get('/checkout', shopController.getCheckout);
-// router.post('/create-order', shopController.postOrder);
+// router.get('/checkout', shopController.getCheckout);
+router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
